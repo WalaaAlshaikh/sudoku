da8730593ac4f54be0a27a952389682486306c5f
walaaalshaikh <walaalshaikh94@gmail.com>
2022-10-19 12:32:17 +0300

    creating the file for Sudoko project

--- lib/main.dart
+++ lib/main.dart
@@ -7,20 +7,31 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Sudoku'),
+      home:  MyHomePage(title: 'Sudoko')
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   final String title;
 
@@ -29,9 +40,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body:
+          Center(child: Column(children: [
+            GridView.count(
+                crossAxisCount: 9,
+                children: List.generate(81, (index) {
+                  return Center(
+                    child: Text('this is '),
+                  );
+                })
+            ),
+          ],
+          ),),
+
+
+
+
+
+           // This trailing comma makes auto-formatting nicer for build methods.
+    );
+
   }
 }
